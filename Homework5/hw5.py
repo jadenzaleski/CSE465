e13aa6d2ca7b964ff6337d61029116b6dad7eabd
Jaden Zaleski <jadenzaleski@icloud.com>
2024-04-17 10:12:10 -0400

    Initial commit of homework 5.

---
+++ Homework5/hw5.py
@@ -0,0 +1,31 @@
+import time
+
+"""
+  Homework#5
+
+  Add your name here: Jaden Zaleski
+  
+  => I’m competing for BONUS Points <=
+
+  You are free to create as many classes within the hw5.py file or across 
+  multiple files as you need. However, ensure that the hw5.py file is the 
+  only one that contains a __main__ method. This specific setup is crucial 
+  because your instructor will run the hw5.py file to execute and evaluate 
+  your work.
+"""
+
+if __name__ == "__main__":
+    start_time = time.perf_counter()  # Do not remove this line
+    '''
+    Inisde the __main__, do not add any codes before this line.
+    -----------------------------------------------------------
+    '''
+
+    '''
+    Inside the __main__, do not add any codes after this line.
+    ----------------------------------------------------------
+    '''
+    end_time = time.perf_counter()
+    # Calculate the runtime in milliseconds
+    runtime_ms = (end_time - start_time) * 1000
+    print(f"The runtime of the program is {runtime_ms} milliseconds.")
