b1381a772c33f8784ded0f87f79759d95b1b97f9
Jaden Zaleski <jadenzaleski@icloud.com>
2024-04-18 22:56:49 -0400

    Verified outputs and added comments. Homework 5 complete.

--- Homework5/record.py
+++ Homework5/record.py
@@ -1,5 +1,6 @@
 # Record Class
 class Record:
+    # Initialization for Record
     def __init__(self, parts):
         self.record_number = parts[0]
         self.zipcode = parts[1]
@@ -8,9 +9,11 @@ class Record:
         self.lat = float(parts[6]) if parts[6] != '' else None
         self.lng = float(parts[7]) if parts[7] != '' else None
 
+    # Override string method
     def __str__(self):
         return f'{self.record_number} {self.city} {self.state} {self.lat} {self.lng}'
 
+    # Override logical ==
     def __eq__(self, other):
         if isinstance(other, Record):
             return (
@@ -24,8 +27,10 @@ class Record:
         else:
             return False
 
+    # See if two Records are equal in regard to zipcodes
     def same_zipcode(self, other):
         return self.zipcode == other.zipcode
 
+    # Make the Record hashable
     def __hash__(self):
         return hash((self.record_number, self.zipcode, self.city, self.state, self.lat, self.lng))
